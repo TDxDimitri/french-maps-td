f95a4b09ee08f792b2f3b0eb9676f82f552f61ef
TDxDimitri <talleu.dimitri@outlook.fr>
2023-02-21 16:09:12 +0100

    version final

--- script/script.js
+++ script/script.js
@@ -1,14 +1,18 @@
 const regionsPath = document.querySelectorAll('path')
-
+var click = 0;
 let listOfregions = []
+var gameEnded = false;
 
 regionsPath.forEach(regionName => {
     
     const title = regionName.getAttribute('title');
-    
     listOfregions.push(title);
 
     regionName.addEventListener('click', (event) => {
+        if (gameEnded) {
+            return;
+        }
+        click = click + 1;
 
         document.querySelector('p').innerHTML= title;
         
@@ -18,14 +22,21 @@ regionsPath.forEach(regionName => {
             event.target.classList.remove('hover')
             event.target.classList.add('nop')
         }
+        else if (title != random) {
+            event.target.classList.remove('land')
+            event.target.classList.remove('hover')
+            event.target.classList.add('nop')
+        }
         else{
             event.target.classList.remove('land')
             event.target.classList.remove('hover')
             event.target.classList.add('win')
+            document.querySelector('.play').innerHTML='<a href="index.html" class="btn">Rejouez</a>';         
             document.querySelector('h3').innerHTML= 'Bravo ! Vous avez trouvez la région mystère en '+click+' coups';
             document.querySelector('h1').classList.add('d-none');            
-            document.querySelector('.res').classList.add('d-none');            
-            
+            document.querySelector('.res').classList.add('d-none');
+            regionName.removeEventListener ('click', regionName.event);         
+            gameEnded = true;
         }
     });
 });
@@ -34,15 +45,5 @@ regionsPath.forEach(regionName => {
 const random = listOfregions[Math.floor(Math.random()*listOfregions.length)];
 console.log (random)
 
-// compteur de click
-var click = 0;
-var Carte = document.querySelector('.map');
-
-Carte.addEventListener('click', function() {
-    click = click + 1;
-    if (click == 5) {
-        alert ('vous avez cliquez '+click+' fois');     
-    }
-});
 
 
